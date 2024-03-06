495553962e9e77598cae4934bfa6f258b08f2e20
jemitchppu <122327944+jemitchppu@users.noreply.github.com>
2024-03-05 19:27:09 -0500

    Update db.js
    switched login info to have online capibility

--- src/teams/db.js
+++ src/teams/db.js
@@ -1,14 +1,13 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    //user: "jemitch",
-    user: "postgres",
-    host: "localhost",
-    //host:"dpg-cn5uefq1hbls73akre80-a",
-    //password: "rlR3HqZbeNqlmPyLRU8GrSwpCucgnCk0",
-    password: "Kadpmu08?",
-    //database: "teams_ul9e",
-    database: "teams",
+    user: "jemitch",
+    //user: "postgres",
+    //host: "localhost",
+    host:"dpg-cn5uefq1hbls73akre80-a",
+    password: "rlR3HqZbeNqlmPyLRU8GrSwpCucgnCk0",
+    database: "teams_ul9e",
+    //database: "teams",
     port: 5432,
 });
 
